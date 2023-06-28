2700d30e3e790ca3c4258d5dd850a0877f328b45
syket-das <saiketsd23@gmail.com>
2023-06-28 23:55:47 +0530

    add package

--- app/components/FeaturedServices.jsx
+++ app/components/FeaturedServices.jsx
@@ -1,3 +1,4 @@
+'use client';
 import React from 'react';
 
 const FeaturedServices = () => {
@@ -8,7 +9,7 @@ const FeaturedServices = () => {
           Featured Services
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 2xl:grid-cols-4 gap-4">
-          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((item, index) => {
+          {[1, 2, 3, 4, 5, 6, 7, 8].map((item, index) => {
             return (
               <div
                 key={index}
@@ -24,7 +25,7 @@ const FeaturedServices = () => {
                   <h2 className="card-title">Shoes!</h2>
                   <p>If a dog chews shoes whose shoes does he choose?</p>
                   <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+                    <button className="btn glass">Buy Now</button>
                   </div>
                 </div>
               </div>
@@ -34,13 +35,15 @@ const FeaturedServices = () => {
         <div className=" mt-8 flex justify-center ">
           <div className=" flex items-center gap-2 cursor-pointer hover:underline">
             <p className="text-gray-500 hover:underline">View All Services</p>
-            <svg className="w-6 h-6 text-gray-500 animate-bounce" viewBox="0 0 24 24">
-                <path
-                    d="M16.59 8.59L12 13.17L7.41 8.59L6 10L12 16L18 10L16.59 8.59Z"
-                    fill="currentColor"
-                ></path>
+            <svg
+              className="w-6 h-6 text-gray-500 animate-bounce"
+              viewBox="0 0 24 24"
+            >
+              <path
+                d="M16.59 8.59L12 13.17L7.41 8.59L6 10L12 16L18 10L16.59 8.59Z"
+                fill="currentColor"
+              ></path>
             </svg>
-
           </div>
         </div>
       </div>
