015b0f6cc7667fb6d1ca4b394c2be1b373a7960c
syket-das <saiketsd23@gmail.com>
2023-06-29 11:26:32 +0530

    categories

--- app/(pages)/categories/page.jsx
+++ app/(pages)/categories/page.jsx
@@ -97,15 +97,66 @@ const page = () => {
   ]);
   return (
     <ClientOnly>
-      <div className="mx-4">
-        <div className="flex justify-center ">
+      <div className="mx-4 my-8">
+        <div className="flex justify-center flex-col gap-16">
+          <form>
+            <div className="flex items-center gap-4">
+              <h1 className="text-lg md:text-2xl mb-4 font-extrabold text-white">
+                Categories
+              </h1>
+              <p className="text-gray-400">9 available </p>
+            </div>
+
+            <label
+              for="search"
+              className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+            >
+              Search
+            </label>
+            <div className="relative max-w-full  md:max-w-[500px] ">
+              <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <svg
+                  aria-hidden="true"
+                  className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  ></path>
+                </svg>
+              </div>
+              <input
+                type="search"
+                id="search"
+                className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Search"
+                required
+              />
+              <button
+                type="submit"
+                className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Search
+              </button>
+            </div>
+          </form>
+
           <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 gap-4 ">
             {categories.map((category) => (
-              <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700
+              <div
+                key={category.id}
+                className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700
               hover:shadow-lg hover:border-gray-300 dark:hover:border-gray-500 cursor-pointer
-              ">
+              "
+              >
                 <a href="#">
-                  <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white text-center">
+                  <h5 className="mb-2 text-sm md:text-lg  font-semibold tracking-tight text-gray-900 dark:text-white text-center">
                     {category.name}
                   </h5>
                 </a>
@@ -135,10 +186,7 @@ const page = () => {
 
                   <div className="flex gap-3 items-center mt-3">
                     <div className="flex-1">
-                      <p className="text-xs text-gray-500"> Price</p>
-                    </div>
-                    <div className="flex-1">
-                      <p className="text-xs text-gray-500">
+                      <p className="text-xs  text-center">
                         {' '}
                         ${category.minOfferingPrice} - $
                         {category.maxOfferingPrice}
